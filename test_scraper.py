517812cedd2231e7b52234a9165bd9ab739e2f4a
Corey Pieper <26394708+coreyjpieper@users.noreply.github.com>
2020-10-17 19:05:56 -0500

    test: Add second test and improve documentation

--- test_scraper.py
+++ test_scraper.py
@@ -8,10 +8,40 @@ course_scraper = importlib.import_module("course_scraper")
 
 
 def test_more_than_one_prefix(courses):
-    """Debug manually"""
+    """
+    Find courses where there is one or fewer prefixes listed. This should not be possible because each
+    cross-listed course should have at least two prefixes.
+
+    Usually this is caused by invalid cross-listed course numbers in the class schedule causing the regex
+    to not pick up the course number, for example:
+
+        *Cross-listed with MCST 2904-03*    # should be MCST 294-03
+    """
     one_cross_list = {k: v for k, v in courses.items() if len(v["prefixes"]) <= 1}
     for i in one_cross_list:
         print(i, one_cross_list[i])
+
+
+def test_prefixes_in_order(courses):
+    """
+    Find courses where the prefixes are not listed in alphabetical order. This should not be possible
+    because the courses on the class schedule are listed and read alphabetically, and in the dictionary the course
+    number comes before the cross-listed courses numbers.
+
+    Usually this is caused by errors in the class schedule such as the same course being listed under different
+    names, for example:
+
+        Introduction to Analysis of Hispanic Texts
+        Introduction to the Analysis of Hispanic Texts
+
+        Queer and Trans Oral Hist Proj
+        Queer and Trans Oral History Project
+
+    or a cross-listed course missing from the description.
+    """
+    prefixes_out_of_order = {k: v for k, v in courses.items() if v["prefixes"] != sorted(v["prefixes"])}
+    for i in prefixes_out_of_order:
+        print(i, str(prefixes_out_of_order[i]))
 
 
 if __name__ == '__main__':
@@ -21,3 +51,5 @@ if __name__ == '__main__':
 
     cross_listed_courses = course_scraper.get_cross_listed(bs4_content)
     test_more_than_one_prefix(cross_listed_courses)
+    print("\n\n")
+    test_prefixes_in_order(cross_listed_courses)
