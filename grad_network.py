feb936b5add4ade28a2ae38269815f2a4208f4ab
Corey Pieper <26394708+coreyjpieper@users.noreply.github.com>
2020-10-19 20:46:52 -0500

    feat: Update txt cleaning function
    Now it only writes every 3rd line, which are the lines containing major
    information from students

--- grad_network.py
+++ grad_network.py
@@ -18,7 +18,10 @@ def clean_commencement_text(in_file, out_file):
                                         (.* (?:Major|Minor|\b and\b|Concentration) .*)\n  # match the second line
                                         (.* (?:Major|Minor|Concentration) )? (\n)?        # match the third, optional line                                        
                                     """, r"\1 \2 \3\n", text)   # put the lines all on one line
+
     print(f"{num_subs} substitutions made")
+    # get every third line from the text, the extra "\n" appended to new_text is needed to include the last person
+    new_text = re.sub(r"(?x) .*\n .*\n (.*\n)", r"\1", new_text + "\n")
 
     with open(out_file, 'w', encoding="utf-8") as f:
         f.write(new_text)
