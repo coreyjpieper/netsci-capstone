5f6090d3305a7bf7cda57de06013baf7e7aa4874
Corey Pieper <26394708+coreyjpieper@users.noreply.github.com>
2020-10-20 11:53:41 -0500

    style: Improve comments in clean function
    Moved around and clarified some comments

--- grad_network.py
+++ grad_network.py
@@ -9,22 +9,22 @@ def create_nodes(in_file, out_file):
 
 def clean_commencement_text(in_file, out_file):
     new_text: str
-
     with open(in_file, 'r', encoding="utf-8") as f:
         text = f.read()
-        # the same regex w/o whitespace is:
-        # "(.*(?:Major|Minor).*)\n(.*(?:Major|Minor|\band\b|Concentration).*)\n(.*(?:Major|Minor|Concentration))?(\n)?"
+        # combine lines with major/minor/concentration info into one line
         new_text, num_subs = re.subn(r"""(?x) (.* (?:Major|Minor) .*)\n                   # match the first line
                                         (.* (?:Major|Minor|\b and\b|Concentration) .*)\n  # match the second line
-                                        (.* (?:Major|Minor|Concentration) )? (\n)?        # match the third, optional line                                        
-                                    """, r"\1 \2 \3\n", text)   # put the lines all on one line
+                                        (.* (?:Major|Minor|Concentration) )? (\n)?      # match the third, optional line
+                                    """, r"\1 \2 \3\n", text)                           # put the lines all on one line
+        # the same regex without whitespace is:
+        # "(.*(?:Major|Minor).*)\n(.*(?:Major|Minor|\band\b|Concentration).*)\n(.*(?:Major|Minor|Concentration))?(\n)?"
 
     print(f"{num_subs} substitutions made")
-    # get every third line from the text, the extra "\n" appended to new_text is needed to include the last person
-    new_text = re.sub(r"(?x) .*\n .*\n (.*\n)", r"\1", new_text + "\n")
+    # get every third line from the text, these correspond to the lines with degree info
+    degree_info = new_text.splitlines(keepends=True)[2::3]
 
     with open(out_file, 'w', encoding="utf-8") as f:
-        f.write(new_text)
+        f.write("".join(degree_info))
 
 
 clean_commencement_text("commencement_2020.txt", "commencement_2020_clean.txt")
