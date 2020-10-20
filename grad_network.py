5320a1865be692658e69fdc3e0986007c4efcbe7
Corey Pieper <26394708+coreyjpieper@users.noreply.github.com>
2020-10-19 20:31:45 -0500

    feat: Add grad_network.py and cleaning function
    The function `clean_commencement_text` combines multi-line majors into a
    single line so that lines with major information occur at regular
    intervals

---
+++ grad_network.py
@@ -0,0 +1,27 @@
+from pprint import pp
+import re
+
+
+def create_nodes(in_file, out_file):
+    # result = content.find_all("h3", id=True)
+    pass
+
+
+def clean_commencement_text(in_file, out_file):
+    new_text: str
+
+    with open(in_file, 'r', encoding="utf-8") as f:
+        text = f.read()
+        # the same regex w/o whitespace is:
+        # "(.*(?:Major|Minor).*)\n(.*(?:Major|Minor|\band\b|Concentration).*)\n(.*(?:Major|Minor|Concentration))?(\n)?"
+        new_text, num_subs = re.subn(r"""(?x) (.* (?:Major|Minor) .*)\n                   # match the first line
+                                        (.* (?:Major|Minor|\b and\b|Concentration) .*)\n  # match the second line
+                                        (.* (?:Major|Minor|Concentration) )? (\n)?        # match the third, optional line                                        
+                                    """, r"\1 \2 \3\n", text)   # put the lines all on one line
+    print(f"{num_subs} substitutions made")
+
+    with open(out_file, 'w', encoding="utf-8") as f:
+        f.write(new_text)
+
+
+clean_commencement_text("commencement_2020.txt", "commencement_2020_clean.txt")
